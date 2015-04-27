dfa2022b91b3cab1d84ee94aa4e608fec489bfca
Jason S. Jones <jsjones96@gmail.com>
2015-04-27 06:15:00 -0700

    complete basic API and tests

--- test/hashtable.spec.js
+++ test/hashtable.spec.js
@@ -18,7 +18,23 @@ describe('Hash Table', function () {
     });
 
     it('hashes a key and puts the value in the hash table', function () {
-        ht.put('jason', 'me@myemail.com');
+        ht.put('node', 'asychronous, event-driven io for server side javascript');
         expect(ht.isEmpty()).to.not.be.true;
+    });
+
+    it('gets the correct value when given the key', function () {
+        ht.put('node', 'server-side js');
+        ht.put('mongodb', 'noSQL database');
+        expect(ht.isEmpty()).to.not.be.true;
+        expect(ht.get('node')).to.equal('server-side js');
+        expect(ht.get('mongodb')).to.equal('noSQL database');
+    });
+
+    it('removes the correct value from the hash table', function () {
+        ht.put('node', 'server-side js');
+        ht.put('mongodb', 'noSQL database');
+        expect(ht.isEmpty()).to.not.be.true;
+        expect(ht.remove('node')).to.be.undefined;
+        expect(ht.remove('mongodb')).to.be.undefined;
     });
 });
