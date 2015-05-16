94b5c8a21bb9abb68b308e2a792089b425e92ebf
Jason Jones <jsjones96@gmail.com>
2015-05-16 04:27:37 +0000

    restructure test scenarios

--- test/hashtable.spec.js
+++ test/hashtable.spec.js
@@ -17,85 +17,98 @@ describe('A Hash Table', function () {
         expect(ht.isEmpty()).to.be.true;
     });
 
-    it('hashes a key and puts the value in the hash table', function () {
-        ht.put('node', 'asychronous, event-driven io for server side javascript');
-        expect(ht.isEmpty()).to.be.false;
-    });
+    describe('hashes a key and puts the value in the hash table', function () {
 
-    it('hashes a key and puts the value in the hash table using separate chaining', function () {
-        ht.scPut('node', 'asychronous, event-driven io for server side javascript');
-        expect(ht.isEmpty()).to.be.false;
-    });
+        it('with no collision handling', function () {
+            ht.put('node', 'asychronous, event-driven io for server side javascript');
+            expect(ht.isEmpty()).to.be.false;
+        });
 
-    it('gets the correct value when given the key', function () {
-        ht.put('node', 'server-side js');
-        ht.put('mongodb', 'noSQL database');
-        expect(ht.isEmpty()).to.be.false;
-        expect(ht.get('node')).to.equal('server-side js');
-        expect(ht.get('mongodb')).to.equal('noSQL database');
-    });
+        it('using separate chaining', function () {
+            ht.scPut('node', 'asychronous, event-driven io for server side javascript');
+            expect(ht.isEmpty()).to.be.false;
+        });
 
-    it('gets the correct value when given the key using separate chaining', function () {
-        ht.scPut('node', 'server-side js');
-        ht.scPut('mongodb', 'noSQL database');
-        expect(ht.isEmpty()).to.be.false;
-        expect(ht.scGet('node')).to.equal('server-side js');
-        expect(ht.scGet('mongodb')).to.equal('noSQL database');
-    });
+        it('using the fluent/cascading API', function () {
+            ht.put('node', 'server-side js')
+              .put('mongodb', 'noSQL database');
+            expect(ht.isEmpty()).to.be.false;
+            expect(ht.get('node')).to.equal('server-side js');
+            expect(ht.get('mongodb')).to.equal('noSQL database');
 
-    it('returns -1 if asked to get a key that does not exist in the hash table', function () {
-        ht.scPut('node', 'server-side js');
-        ht.scPut('mongodb', 'noSQL database');
-        expect(ht.isEmpty()).to.be.false;
-        expect(ht.scGet('node')).to.equal('server-side js');
-        expect(ht.scGet('express')).to.equal(-1);
-    });
-
-    it('gets the last assigned value for a given key when using separate chaining', function () {
-        ht.scPut('node', 'bogus value');
-        ht.scPut('node', 'server-side js');
-        ht.scPut('mongodb', 'noSQL database');
-        expect(ht.isEmpty()).to.be.false;
-        expect(ht.scGet('node')).to.equal('server-side js');
-        expect(ht.scGet('mongodb')).to.equal('noSQL database');
-    });
-
-    it('correctly chains calls to the put method for a fluent/cascading API', function () {
-        ht.put('node', 'server-side js')
-          .put('mongodb', 'noSQL database');
-        expect(ht.isEmpty()).to.be.false;
-        expect(ht.get('node')).to.equal('server-side js');
-        expect(ht.get('mongodb')).to.equal('noSQL database');
+        });
 
     });
 
-    it('removes the correct value from the hash table', function () {
-        ht.put('node', 'server-side js');
-        ht.put('mongodb', 'noSQL database');
-        expect(ht.isEmpty()).to.be.false;
-        ht.remove('node');
-        ht.remove('mongodb');
-        expect(ht.get('node')).to.be.undefined;
-        expect(ht.get('mongodb')).to.be.undefined;
+    describe('gets the correct value when given a key', function () {
+
+        it('with no collision handling', function () {
+            ht.put('node', 'server-side js');
+            ht.put('mongodb', 'noSQL database');
+            expect(ht.isEmpty()).to.be.false;
+            expect(ht.get('node')).to.equal('server-side js');
+            expect(ht.get('mongodb')).to.equal('noSQL database');
+        });
+
+        it('using separate chaining', function () {
+            ht.scPut('node', 'server-side js');
+            ht.scPut('mongodb', 'noSQL database');
+            expect(ht.isEmpty()).to.be.false;
+            expect(ht.scGet('node')).to.equal('server-side js');
+            expect(ht.scGet('mongodb')).to.equal('noSQL database');
+        });
+
+        it('that does not exist in the hash table', function () {
+            ht.scPut('node', 'server-side js');
+            ht.scPut('mongodb', 'noSQL database');
+            expect(ht.isEmpty()).to.be.false;
+            expect(ht.scGet('node')).to.equal('server-side js');
+            expect(ht.scGet('express')).to.equal(-1);
+        });
+
+        it('gets the last assigned value for a given key when using separate chaining',
+            function () {
+                ht.scPut('node', 'bogus value');
+                ht.scPut('node', 'server-side js');
+                ht.scPut('mongodb', 'noSQL database');
+                expect(ht.isEmpty()).to.be.false;
+                expect(ht.scGet('node')).to.equal('server-side js');
+                expect(ht.scGet('mongodb')).to.equal('noSQL database');
+            });
+
     });
 
-    it('correctly chains calls to the remove method for a fluent/cascading API', function () {
-        ht.put('node', 'server-side js')
-          .put('mongodb', 'noSQL database');
-        expect(ht.isEmpty()).to.be.false;
-        ht.remove('node')
-          .remove('mongodb');
-        expect(ht.get('node')).to.be.undefined;
-        expect(ht.get('mongodb')).to.be.undefined;
+    describe('removes the correct value from the hash table', function () {
+        it('with no collison handling', function () {
+            ht.put('node', 'server-side js');
+            ht.put('mongodb', 'noSQL database');
+            expect(ht.isEmpty()).to.be.false;
+            ht.remove('node');
+            ht.remove('mongodb');
+            expect(ht.get('node')).to.be.undefined;
+            expect(ht.get('mongodb')).to.be.undefined;
+        });
+
+        it('using the fluent/cascading API', function () {
+            ht.put('node', 'server-side js')
+              .put('mongodb', 'noSQL database');
+            expect(ht.isEmpty()).to.be.false;
+            ht.remove('node')
+              .remove('mongodb');
+            expect(ht.get('node')).to.be.undefined;
+            expect(ht.get('mongodb')).to.be.undefined;
+        });
+
+        it('using separate chaining', function () {
+            ht.scPut('node', 'server-side js');
+            expect(ht.isEmpty()).to.be.false;
+            expect(ht.scRemove('node')).to.be.true;
+            expect(ht.scGet('node')).to.equal(-1);
+        });
+
+        it('removes all the keys from the hash table when using separate chaining');
+        it('returns false if asked to remove a key from the hash table that does not exist');
+
     });
 
-    it('removes the key from the hash table when using separate chaining', function () {
-        ht.scPut('node', 'server-side js');
-        expect(ht.isEmpty()).to.be.false;
-        expect(ht.scRemove('node')).to.be.true;
-        expect(ht.scGet('node')).to.equal(-1);
-    });
-
-    it('removes all the keys from the hash table when using separate chaining');
-    it('returns false if asked to remove a key from the hash table that does not exist');
 });
