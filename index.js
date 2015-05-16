9b873ca45fa769fda570e2e1e86ea5818fec1888
Jason Jones <jsjones96@gmail.com>
2015-05-16 05:16:53 +0000

    refactor prototype

--- index.js
+++ index.js
@@ -70,138 +70,136 @@
      * savings since there can be several different hash table instances
      * instantiated.
      */
-    HashTable.prototype = {
 
-        /**
-         * Determines if the hash table is empty
-         *
-         * @returns {boolean} true if hash table is empty, false otherwise
-         */
-        isEmpty: function () {
-            return this.table.length === 0;
-        },
+    /**
+     * Determines if the hash table is empty
+     *
+     * @returns {boolean} true if hash table is empty, false otherwise
+     */
+    HashTable.prototype.isEmpty  = function () {
+        return this.table.length === 0;
+    };
 
-        /**
-         * Puts the value in the hash table.  Hashes the key to determine
-         * the index in the hash table to assign the value.
-         *
-         * @param {string} key the key to hash to determine the index
-         * @param {number|string|object} value the value to associate with
-         *          the key/hash in the hash table
-         *
-         * @returns {object} this for method chaining
-         */
-        put: function (key, value) {
-            var index = this.hashFn(key);
-            this.table[index] = value;
-            return this;
-        },
+    /**
+     * Puts the value in the hash table.  Hashes the key to determine
+     * the index in the hash table to assign the value.
+     *
+     * @param {string} key the key to hash to determine the index
+     * @param {number|string|object} value the value to associate with
+     *          the key/hash in the hash table
+     *
+     * @returns {object} this for method chaining
+     */
+    HashTable.prototype.put = function (key, value) {
+        var index = this.hashFn(key);
+        this.table[index] = value;
+        return this;
+    };
 
-        /**
-         * Puts the value in the hash table utilizing separate chaining to
-         * handle hash collisions.  Hashes the key to determine
-         * the index of the hash table and inserts a key/value object in the
-         * front of the linked-list at the index.
-         *
-         * @param {string} key the key to hash to determine the index
-         * @param {number|string|object} value the value to associate with
-         *          the key/hash in the hash table
-         *
-         * @returns {object} this for method chaining
-         */
-        scPut: function (key, value) {
-            var index = this.hashFn(key);
-            if (this.table[index] === undefined) {
-                this.table[index] = new LinkedList();
-            }
-
-            // append new key/value pair (obj) to the front of the linked list.
-            // adding the data to the front of the list will ensure that we
-            // return the latest value from the list in the event there are
-            // duplicate keys with differing values
-            this.table[index].insertFirst(new ValuePair(key, value));
-            return this;
-        },
-
-        /**
-         * Gets the value in the hash table based on the hash of the key
-         *
-         * @param {string} key the key to hash to determine the index to
-         *                 retrieve the value
-         * @returns {number|string|object} the value associated the the key
-         */
-        get: function (key) {
-            return this.table[this.hashFn(key)];
-        },
-
-        /**
-         * Gets the value in the hash table associated with the key.  This
-         * version utilizes separate chaining to search for the key in the
-         * respective linked-list.
-         *
-         * @param {string} key the key to hash to determine the index to
-         *                 retrieve the value
-         * @returns {number|string|object} the value associated the the key
-         */
-        scGet: function (key) {
-            var index = this.hashFn(key);
-            if (this.table[index] === undefined) {
-                return -1;
-            }
-
-            // initially set the current node to the head of the list
-            var current = this.table[index].getHeadNode();
-
-            // iterate over the list
-            while (current !== null) {
-
-                // until the first keys match
-                if (current.getData().key === key) {
-                    return current.getData().value;
-                }
-
-                // no match, so get the next node in the list
-                current = current.next;
-            }
-
-            // if we get here, there is not a node in the list with the key,
-            // maybe because it was previously removed
-            return -1;
-        },
-
-        /**
-         * Removes the value in the hash table associated with the key
-         *
-         * @param {string} key the key to hash to determine the index to
-         *                 remove the value
-         *
-         * @returns {object} this for method chaining
-         */
-        remove: function (key) {
-            this.table[this.hashFn(key)] = undefined;
-            return this;
-        },
-
-        scRemove: function (key) {
-            var index = this.hashFn(key);
-
-            if (this.table[index] === undefined) {
-                return false;
-            }
-
-            var current = this.table[index].getHeadNode();
-            var listIndex = 0;
-            var status = false;
-            while (listContainsKey(this.table[index], key) && current !== null) {
-                if (current.getData().key === key) {
-                    this.table[index].removeAt(listIndex);
-                    status = true;
-                }
-                current = current.next;
-                listIndex++;
-            }
-            return status;
+    /**
+     * Puts the value in the hash table utilizing separate chaining to
+     * handle hash collisions.  Hashes the key to determine
+     * the index of the hash table and inserts a key/value object in the
+     * front of the linked-list at the index.
+     *
+     * @param {string} key the key to hash to determine the index
+     * @param {number|string|object} value the value to associate with
+     *          the key/hash in the hash table
+     *
+     * @returns {object} this for method chaining
+     */
+    HashTable.prototype.scPut = function (key, value) {
+        var index = this.hashFn(key);
+        if (this.table[index] === undefined) {
+            this.table[index] = new LinkedList();
         }
+
+        // append new key/value pair (obj) to the front of the linked list.
+        // adding the data to the front of the list will ensure that we
+        // return the latest value from the list in the event there are
+        // duplicate keys with differing values
+        this.table[index].insertFirst(new ValuePair(key, value));
+        return this;
+    };
+
+    /**
+     * Gets the value in the hash table based on the hash of the key
+     *
+     * @param {string} key the key to hash to determine the index to
+     *                 retrieve the value
+     * @returns {number|string|object} the value associated the the key
+     */
+    HashTable.prototype.get = function (key) {
+        return this.table[this.hashFn(key)];
+    };
+
+    /**
+     * Gets the value in the hash table associated with the key.  This
+     * version utilizes separate chaining to search for the key in the
+     * respective linked-list.
+     *
+     * @param {string} key the key to hash to determine the index to
+     *                 retrieve the value
+     * @returns {number|string|object} the value associated the the key
+     */
+    HashTable.prototype.scGet = function (key) {
+        var index = this.hashFn(key);
+        if (this.table[index] === undefined) {
+            return -1;
+        }
+
+        // initially set the current node to the head of the list
+        var current = this.table[index].getHeadNode();
+
+        // iterate over the list
+        while (current !== null) {
+
+            // until the first keys match
+            if (current.getData().key === key) {
+                return current.getData().value;
+            }
+
+            // no match, so get the next node in the list
+            current = current.next;
+        }
+
+        // if we get here, there is not a node in the list with the key,
+        // maybe because it was previously removed
+        return -1;
+    };
+
+    /**
+     * Removes the value in the hash table associated with the key
+     *
+     * @param {string} key the key to hash to determine the index to
+     *                 remove the value
+     *
+     * @returns {object} this for method chaining
+     */
+    HashTable.prototype.remove = function (key) {
+        this.table[this.hashFn(key)] = undefined;
+        return this;
+    };
+
+    HashTable.prototype.scRemove = function (key) {
+        var index = this.hashFn(key);
+
+        if (this.table[index] === undefined) {
+            return false;
+        }
+
+        var current = this.table[index].getHeadNode();
+        var listIndex = 0;
+        var status = false;
+        while (listContainsKey(this.table[index], key) && current !== null) {
+            if (current.getData().key === key) {
+                this.table[index].removeAt(listIndex);
+                status = true;
+            }
+            current = current.next;
+            listIndex++;
+        }
+        return status;
     };
 
     // expose HashTable
